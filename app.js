9f3c684c3ad074da96fa30db65c4adfa5c5dd8ce
Etienne Bovet <ebovet@gmail.com>
2019-11-28 15:47:29 +0800

    Now changed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ App({
   },
 
   globalData: {
-    url: `http://localhost:3000/api/v1/`
-    // url: `https://airlumni.herokuapp.com/api/v1/`
+    // url: `http://localhost:3000/api/v1/`
+    url: `https://airlumni.herokuapp.com/api/v1/`
     }
 })
